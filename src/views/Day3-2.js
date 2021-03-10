fdeddecd44e639bda29d00a9de39be64b8112163
pannachow <chowpanna@gmail.com>
2021-03-10 15:45:33 +0100

    add more cards

--- src/views/Day3-2.js
+++ src/views/Day3-2.js
@@ -1,9 +1,189 @@
+import { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import DoneIcon from "@material-ui/icons/Done";
+import IconButton from "@material-ui/core/IconButton";
+
+const styles = {
+  title1: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#A9F6FF",
+    fontWeight: 700,
+    fontSize: "72px",
+    lineHeight: "108px",
+    paddingTop: "50px",
+  },
+  title1_1: {
+    color: "#02E8E8",
+  },
+  title2: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#3487C0",
+    fontWeight: 550,
+    fontSize: "36px",
+    lineHeight: "54px",
+  },
+  title3: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#3487C0",
+    fontWeight: 500,
+    fontSize: "24px",
+    lineHeight: "36px",
+  },
+  title4: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#04E8E8",
+    fontWeight: 600,
+    fontSize: "72px",
+    lineHeight: "108px",
+  },
+  title5: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#0C4D7A",
+    fontWeight: 500,
+    fontSize: "24px",
+    lineHeight: "36px",
+  },
+  title6: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#1B5782",
+    fontWeight: 550,
+    fontSize: "36px",
+    lineHeight: "54px",
+    paddingTop: "20px",
+  },
+  textArea: {
+    width: "100%",
+    height: "91px",
+    left: "193px",
+    top: "1477px",
+    background: "#A9F6FF",
+    borderRadius: "20px",
+  },
+  button: {
+    backgroundColor: "#660362",
+    color: "white",
+    float: "right",
+    fontWeight: 600,
+  },
+};
 
 export default function Day32() {
+  const [choice, setChoice] = useState(null);
+
   return (
-    <Typography align="center" variant="h1" gutterBottom>
-      Card 2
-    </Typography>
+    <>
+      <Box alignItems="left">
+        <Typography variant="h2" style={styles.title1} gutterBottom>
+          DAY 3 : <span style={styles.title1_1}>Card 2</span>
+        </Typography>
+
+        <Typography style={styles.title2}>
+          3 research articles, which one is research-informed?
+        </Typography>
+
+        <Typography style={styles.title3} gutterBottom>
+          Open the three links for the articles and find out which one is an evidence based practice
+          / resources. Mark your answer and let us know.
+        </Typography>
+      </Box>
+
+      <Grid container spacing={1} alignItems="center">
+        <Row
+          number={1}
+          selected={choice}
+          setSelected={setChoice}
+          label="Article 1 - 5 Signs That a Classroom Is Inclusive | Reading Rockets"
+          value="1"
+          link="https://www.readingrockets.org/article/5-signs-classroom-inclusive"
+        />
+        <Row
+          number={2}
+          selected={choice}
+          setSelected={setChoice}
+          label="Article 2 - 50 Tips and Tricks to Facilitating a More Inclusive Classroom | Weareteachers.com"
+          value="2" 
+          link="https://www.weareteachers.com/tips-tricks-inclusive-classroom/"
+        />
+        <Row
+          number={3}
+          selected={choice}
+          setSelected={setChoice}
+          label="Article 3 - Teacher education for inclusion: Can a virtual learning object help? | ScienceDirect"
+          value="3" 
+          link="https://www.sciencedirect.com/science/article/abs/pii/S0360131515000706?casa_token=AaRqWh28zoYAAAAA:gGQc-Uy7QOn4UGAe5sdTW0Lk9Pp3KvURd4Jt2rFDAKIPSb9F0-31C9ikZqflL7B3giIPIkHIiw"
+        />
+      </Grid>
+
+      <br />
+      <Box>
+        <Typography style={styles.title6}>Name</Typography>
+        <input type="text" style={styles.textArea} />
+      </Box>
+      <Box>
+        <Typography style={styles.title6}>Email</Typography>
+        <input type="text" style={styles.textArea} />
+      </Box>
+      <br />
+      <br />
+      <Button variant="contained" style={styles.button} component={RouterLink} to="/submit">
+        SUBMIT
+      </Button>
+    </>
+  );
+}
+
+function Row({ number, selected, setSelected, label, value, link }) {
+  return (
+    <>
+      <Grid item xs={1}>
+        <Typography style={styles.title4}>{number}</Typography>
+      </Grid>
+      <Grid item xs={11}>
+        <Choice
+          selected={selected}
+          setSelected={setSelected}
+          label={label}
+          value={value}
+          link={link}
+        />
+      </Grid>
+    </>
+  );
+}
+
+function Choice({ selected, setSelected, label, value, link }) {
+  const size = "40px";
+  return (
+    <Box display="flex" flexDirection="row" alignItems="center">
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        bgcolor="#FFE843"
+        borderRadius="50%"
+        width={size}
+        height={size}
+        boxShadow={2}
+      >
+        <IconButton style={{ width: size, height: size }} onClick={() => setSelected(value)}>
+          {selected === value && <DoneIcon />}
+        </IconButton>
+      </Box>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Link href={link}>
+        <Typography style={styles.title5}>{label}</Typography>
+      </Link>
+    </Box>
   );
 }
--- src/views/Day4-2.js
+++ src/views/Day4-2.js
@@ -1,9 +1,133 @@
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+
+const styles = {
+  title1: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#A9F6FF",
+    fontWeight: 700,
+    fontSize: "72px",
+    lineHeight: "108px",
+    paddingTop: "50px",
+  },
+  title1_1: {
+    color: "#02E8E8",
+  },
+  title2: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#3487C0",
+    fontWeight: 550,
+    fontSize: "36px",
+    lineHeight: "54px",
+  },
+  title3: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#3487C0",
+    fontWeight: 500,
+    fontSize: "24px",
+    lineHeight: "36px",
+  },
+  title4: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#04E8E8",
+    fontWeight: 600,
+    fontSize: "72px",
+    lineHeight: "108px",
+    textAlign: "center",
+  },
+  title5: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#0C4D7A",
+    fontWeight: 500,
+    fontSize: "24px",
+    lineHeight: "36px",
+  },
+  title6: {
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    color: "#1B5782",
+    fontWeight: 550,
+    fontSize: "36px",
+    lineHeight: "54px",
+    paddingTop: "20px",
+  },
+  textArea: {
+    width: "100%",
+    height: "91px",
+    left: "193px",
+    top: "1477px",
+    background: "#A9F6FF",
+    borderRadius: "20px",
+  },
+  button: {
+    backgroundColor: "#660362",
+    color: "white",
+    float: "right",
+    fontWeight: 600,
+  },
+};
 
 export default function Day42() {
+  const videos = [
+    { number: 1, content: "Watching a video about Evidence-informed Practices (EIP)" },
+    { number: 2, content: "Reading an article on EIP" },
+    { number: 3, content: "Researching an EIP" },
+    { number: 4, content: "Looking up methods and tools with EIP for the creation of high inclusion in a classroom" },
+    { number: 5, content: "Preparation for a new exercise/lecture" },
+    { number: 6, content: "Scrolling through a website for your work as a teacher" },
+  ];
   return (
-    <Typography align="center" variant="h1" gutterBottom>
-      Card 2
-    </Typography>
+    <>
+      <Box>
+        <Typography variant="h2" style={styles.title1} gutterBottom>
+          DAY 4 : <span style={styles.title1_1}>Card 2</span>
+        </Typography>
+
+        <Typography style={styles.title2}>Game time : Slide it.</Typography>
+
+        <Typography style={styles.title3} gutterBottom>
+          Please slide up the amount of time you will be willing to spend on:
+        </Typography>
+      </Box>
+
+      <br />
+
+      <Grid container spacing={6} justify="center" alignItems="center">
+        {videos.map((video) => (
+          <>
+            <Grid item xs={2}>
+              <Typography style={styles.title4}>{video.number}</Typography>
+            </Grid>
+            <Grid item xs={10}>
+              <Typography style={styles.title5}>{video.content}</Typography>
+              <button>This is a slider in the future.</button>
+            </Grid>
+          </>
+        ))}
+      </Grid>
+
+      <br />
+      <Box>
+        <Typography style={styles.title6}>Name</Typography>
+        <input type="text" style={styles.textArea} />
+      </Box>
+      <Box>
+        <Typography style={styles.title6}>Email</Typography>
+        <input type="text" style={styles.textArea} />
+      </Box>
+      <br />
+      <br />
+      <Button variant="contained" style={styles.button} component={Link} to="/submit">
+        SUBMIT
+      </Button>
+    </>
   );
 }
